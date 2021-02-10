656e0d42d4c6955153da4afaf57bd23f88630cad
Daniel Flores <danielflores125@gmail.com>
2021-02-10 15:59:20 -0800

    Made the overall city scores chart sortable by overall scores

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 import CityPicker from './cityPicker.jsx';
 import CityScoresChart from './cityScoresChart.jsx';
-import CityScoresOverallChart from './cityScoresOverallChart.jsx'
+import CityScoresOverallChart from './cityScoresOverallChart.jsx';
 
 const temp = [{ name: 'Ankara', stats: [9.928, 9.125, 3.97, 0, 2.04, 5.29, 5.9, 7.4, 5.7, 2.02, 2.93, 4.09, 4.32, 2.31, 8.63, 4.48, 5.14] }];
 
@@ -30,14 +30,25 @@ class App extends React.Component {
       .catch((err) => console.log(err));
   }
 
+  sortScores(val) {
+    const { cities } = this.state;
+    let temp;
+    if (val === 'ascending') {
+      cities.sort((a, b) => a.overall - b.overall);
+    } else {
+      cities.sort((a, b) => b.overall - a.overall);
+    }
+    this.setState({ cities });
+  }
+
   render() {
     const { cities, cityScores } = this.state;
     return (
       <>
         <CityPicker cities={cities} getCityScores={this.getCityScores} />
         <CityScoresChart cityScores={cityScores} />
-        <button type="button">Ascending</button>
-        <button type="button">Descending</button>
+        <button type="button" onClick={() => this.sortScores('ascending')}>Ascending</button>
+        <button type="button" onClick={() => this.sortScores('descending')}>Descending</button>
         <CityScoresOverallChart cities={cities} />
       </>
     );
