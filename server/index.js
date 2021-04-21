a48ba6da095a9b6680b1b36cb1384ca12c00914f
Daniel Flores <Danielflores125@gmail.com>
2021-04-20 22:34:22 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const port = 3000;
+const port = 80;
 const path = require('path');
 const dbMethods = require('../database/index.js');
 
