401d8c338ccb47dc590b041b426253b2adbe7989
Daniel Flores <Danielflores125@gmail.com>
2021-04-20 18:58:10 -0700

    Update index.js

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://172.17.0.3:27017/CityScoresDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect('mongodb://54.183.223.10:27017/CityScoresDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost/CityScoresDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.once('open', () => {
